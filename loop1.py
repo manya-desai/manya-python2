c36ee2ae08163c8e1b9e5c64ad05d56a1a04661e
Manya Desai <manyadesai30@gmail.com>
2026-03-10 07:52:06 +0530

    Add files via upload

---
+++ loop1.py
@@ -0,0 +1,52 @@
+""" # Print numbers from 1 to 5
+for i in range (1,6):
+    print(i) """
+
+
+""" # Print a message 3 times
+for i in range(3):
+    print("Hello") """
+
+
+""" # Print numbers from 1 to 10
+for i in range (1,10):
+    print(i) """
+
+
+""" # Print even numbers from 1 to 20
+for i in range (1,21):
+    if i % 2 ==0:
+        print(i) """
+
+
+""" # Print odd numbers from 1 to 15
+for i in range (1,21):
+    if i % 2 !=0:
+        print(i) """
+
+
+""" # Print table of 5
+for i in range (1,11):
+    print( " 5 x " , i , " = " , 5 * i) """ 
+
+
+""" # Print character of a string 
+name = "ATMIYA"
+
+for letter in name:
+    print(letter)  """        
+
+
+""" # Sum of numbers from 1 to 5 
+total = 0
+
+for i in range (1,6):
+    total = total + i
+print("Sum is :" , total) """
+
+
+# Print list element
+numbers = [ 10 , 20 , 30 , 40 ]
+
+for n in numbers:
+    print(n)               
